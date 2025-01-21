fa02a72603c28e481bbf64e5c472beeece827342
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2025-01-21 18:42:49 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../index'); 
 
 describe('Node.js App Endpoints', () => {
-  it('GET / should return "Hello, World!"', async () => {
+  it('GET / should return "Hello, World!!!!!!!"', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Hello, World!');
