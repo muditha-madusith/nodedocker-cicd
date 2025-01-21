4a13ede8af21090ba581e559c61cdf9d00d27a97
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2025-01-21 18:43:44 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ describe('Node.js App Endpoints', () => {
   it('GET / should return "Hello, World!!!!!!!"', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello, World!');
+    expect(response.text).toBe('Hello, World!!!!!!!');
   });
 
   it('GET /hello should return "Hello, From hello route!"', async () => {
